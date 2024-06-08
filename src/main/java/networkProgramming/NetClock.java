ba3009d2a00305f963bd91095d57a767a2573fa0
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-06-08 14:01:52 +0900

    サーバーとクライアント

---
+++ src/main/java/networkProgramming/NetClock.java
@@ -0,0 +1,38 @@
+package networkProgramming;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.logging.Logger;
+
+public class NetClock {
+    private static final Logger logger = Logger.getLogger(NetClock.class.getName());
+
+    public static void main(String[] args) {
+        try (ServerSocket servSock = new ServerSocket(6000, 300)) {
+            while (true) {
+                try (Socket sock = servSock.accept();
+                        OutputStream out = sock.getOutputStream()) {
+
+                    Date date = new Date();
+                    String outstr = "\n" + "Hello, this is NetClock Server." + "\n" + date.toString() + "\n" + "Thank you." + "\n";
+                    out.write(outstr.getBytes());
+                    out.write('\n');
+                    out.flush();
+
+                    // Log the request and response
+                    logger.info("Received a connection from " + sock.getInetAddress() + ":" + sock.getPort());
+                    logger.info("Sent response: " + outstr);
+
+                } catch (IOException e) {
+                    logger.severe("クライアント接続の処理中にエラーが発生しました: " + e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            logger.severe("サーバーの起動中にエラーが発生しました: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
---
+++ src/main/java/networkProgramming/ReadNet.java
@@ -0,0 +1,23 @@
+package networkProgramming;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class ReadNet {
+    public static void main(String[] args) throws IOException {
+        byte[] buff = new byte[1024];
+
+        try (Socket readSock = new Socket(args[0], Integer.parseInt(args[1]));
+                InputStream inputStr = readSock.getInputStream()) {
+
+            int n;
+            while ((n = inputStr.read(buff)) != -1) {
+                System.out.write(buff, 0, n);
+            }
+        } catch (Exception e) {
+            System.err.println("ネットワークエラー: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
