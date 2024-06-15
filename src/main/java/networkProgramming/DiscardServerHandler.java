1f8c291a4d14b0fe4d70db7e8a2472be463b7c68
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-06-15 21:08:22 +0900

    動作確認のために受信した内容を吐かせる. nc localhost 8080で接続してなんか入力するとサーバーの出力に出力される

--- src/main/java/networkProgramming/DiscardServerHandler.java
+++ src/main/java/networkProgramming/DiscardServerHandler.java
@@ -3,6 +3,7 @@ package networkProgramming;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 /// Handles a server-side channel.
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
@@ -10,7 +11,15 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         // Discard the received data silently.
-        ((ByteBuf) msg).release();
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
 
     @Override
