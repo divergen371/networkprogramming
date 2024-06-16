292e2e7a9ce7e72ca8abd39112820bebe4ced260
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-06-16 12:43:33 +0900

    ♻️

--- src/main/java/networkProgramming/Telnet1.java
+++ src/main/java/networkProgramming/Telnet1.java
@@ -8,10 +8,10 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Telnet1 {
-    public    OutputStream        serverOutput;
-    public    BufferedInputStream serverInput;
+    private OutputStream        serverOutput;
+    private BufferedInputStream serverInput;
     // prepare for server socket
-    protected Socket              serverSocket;
+    private Socket              serverSocket;
 
     /**
      * メインメソッド。コマンドライン引数を使用してサーバーに接続します。
@@ -19,9 +19,15 @@ public class Telnet1 {
      * @param args コマンドライン引数。args[0]にはホスト名またはIPアドレス、args[1]にはポート番号が含まれます。
      */
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: java Telnet1 <host> <port>");
+            System.exit(1);
+        }
+
         try {
-            Telnet1 t = new Telnet1();
+            var t = new Telnet1();
             t.openConnection(args[0], Integer.parseInt(args[1]));
+            t.mainProc();
         } catch (Exception e) {
             System.err.println("エラー: " + e);
             System.exit(1);
@@ -48,20 +54,15 @@ public class Telnet1 {
      *
      * @throws IOException 入出力エラーが発生した場合
      */
-    public void main_proc() throws IOException {
-        try {
-            StreamConnector stdin_to_socket = new StreamConnector(
-                    System.in,
-                    serverOutput);
-            StreamConnector socket_to_stdout = new StreamConnector(
-                    serverInput,
-                    System.out);
+    public void mainProc() throws IOException {
+        try (var serverOutput = this.serverOutput; var serverInput = this.serverInput) {
+            var stdinToSocket = new StreamConnector(System.in, serverOutput);
+            var socketToStdout = new StreamConnector(serverInput, System.out);
 
-            // Thread create
-            Thread inputThread = new Thread(stdin_to_socket);
-            Thread outputThread = new Thread(socket_to_stdout);
+            // Thread create and start using lambdas for brevity
+            var inputThread = new Thread(stdinToSocket);
+            var outputThread = new Thread(socketToStdout);
 
-            // Thread start
             inputThread.start();
             outputThread.start();
         } catch (Exception e) {
@@ -75,9 +76,10 @@ public class Telnet1 {
  * 入力ストリームから出力ストリームへデータを転送するクラスです。
  * Runnableインターフェースを実装し、別スレッドでの実行をサポートします。
  */
+@SuppressWarnings("InfiniteLoopStatement")
 class StreamConnector implements Runnable {
-    private InputStream  src  = null;
-    private OutputStream dest = null;
+    private final InputStream  src;
+    private final OutputStream dest;
 
     /**
      * 指定された入力ストリームと出力ストリームでStreamConnectorを初期化します。
@@ -96,18 +98,18 @@ class StreamConnector implements Runnable {
      */
     @Override
     public void run() {
-        byte[] buff = new byte[1024];
-        while (true) {
-            try {
-                int n = src.read(buff);
+        var buffer = new byte[1024];
+        try (var src = this.src; var dest = this.dest) {
+            while (true) {
+                int n = src.read(buffer);
                 if (n > 0) {
-                    dest.write(buff, 0, n);
+                    dest.write(buffer, 0, n);
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.err.println("エラー: " + e);
-                System.exit(1);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("エラー: " + e);
+            System.exit(1);
         }
     }
 }
