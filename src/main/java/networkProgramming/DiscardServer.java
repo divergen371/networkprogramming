63d8a69ca78eff939fd30cec0b168b3aa7cc8d82
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-06-16 15:48:25 +0900

    finalに変更

--- src/main/java/networkProgramming/DiscardServer.java
+++ src/main/java/networkProgramming/DiscardServer.java
@@ -11,7 +11,7 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class DiscardServer {
 
-    private int port;
+    private final int port;
 
     public DiscardServer(int port) {
         this.port = port;
